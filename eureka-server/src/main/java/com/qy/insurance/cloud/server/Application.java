9658c38b81f80becdc4af9faee2ab6436791c0b1
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-02-23 13:48:04 +0800

    update

--- eureka-server/src/main/java/com/qy/insurance/cloud/server/Application.java
+++ eureka-server/src/main/java/com/qy/insurance/cloud/server/Application.java
@@ -1,8 +1,10 @@
 package com.qy.insurance.cloud.server;
 
+import com.qy.insurance.cloud.server.config.SSLConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Import;
 
 /**
  * @task:
@@ -11,8 +13,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @date: 2017/2/15 18:22
  * @version: 1.0.0
  */
-@EnableEurekaServer
 @SpringBootApplication
+@Import(SSLConfig.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
