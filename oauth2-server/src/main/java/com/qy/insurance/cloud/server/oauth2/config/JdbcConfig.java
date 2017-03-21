4e96aae7b951cf85999ea0de99c2338363e66bdf
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-03-21 10:45:33 +0800

    BXZL-721 #commit: Update the datasource implementation

--- oauth2-server/src/main/java/com/qy/insurance/cloud/server/oauth2/config/JdbcConfig.java
+++ oauth2-server/src/main/java/com/qy/insurance/cloud/server/oauth2/config/JdbcConfig.java
@@ -1,10 +1,10 @@
 package com.qy.insurance.cloud.server.oauth2.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import com.qy.insurance.cloud.core.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
@@ -19,16 +19,19 @@ import javax.sql.DataSource;
 @Configuration
 public class JdbcConfig {
 
-    @Value("${spring.datasource.type}")
-    private String type;
+    public static final String NAME = "datasource";
+    public static final String PREFIX = "spring." + NAME;
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    @SuppressWarnings("unchecked")
-    public DataSource authDataSource() throws ClassNotFoundException {
-        return DataSourceBuilder.create()
-                .type((Class<? extends DataSource>) Class.forName(type))
-                .build();
+    @ConfigurationProperties(prefix = PREFIX)
+    public DataSourceProperties commonDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = NAME + "DataSource")
+    public DataSource commonDataSource() {
+        return commonDataSourceProperties().generateDatasource();
     }
 
     @Bean
--- zipkin-server/src/main/java/com/qy/insurance/cloud/server/zipkin/config/DbConfig.java
+++ zipkin-server/src/main/java/com/qy/insurance/cloud/server/zipkin/config/DbConfig.java
@@ -1,7 +1,6 @@
 package com.qy.insurance.cloud.server.zipkin.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import com.qy.insurance.cloud.core.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,18 +20,16 @@ public class DbConfig {
 
     public static final String NAME = "datasource";
     public static final String PREFIX = "spring." + NAME;
-    public static final String TYPE = "${" + PREFIX + ".type" + "}";
 
-    @Value(TYPE)
-    private String dsType;
-
-    @Primary
     @Bean
     @ConfigurationProperties(prefix = PREFIX)
-    @SuppressWarnings("unchecked")
-    public DataSource dataSource() throws ClassNotFoundException {
-        return DataSourceBuilder.create()
-                .type((Class<? extends DataSource>) Class.forName(dsType))
-                .build();
+    public DataSourceProperties commonDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = NAME + "DataSource")
+    public DataSource commonDataSource() {
+        return commonDataSourceProperties().generateDatasource();
     }
 }
