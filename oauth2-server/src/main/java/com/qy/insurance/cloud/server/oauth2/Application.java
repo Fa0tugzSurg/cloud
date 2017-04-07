431730cc9553d97bbe3faae4d140ae830c6617e3
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-04-07 18:26:53 +0800

    接入cloud

--- oauth2-server/src/main/java/com/qy/insurance/cloud/server/oauth2/Application.java
+++ oauth2-server/src/main/java/com/qy/insurance/cloud/server/oauth2/Application.java
@@ -2,6 +2,7 @@ package com.qy.insurance.cloud.server.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  * @version: 1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
