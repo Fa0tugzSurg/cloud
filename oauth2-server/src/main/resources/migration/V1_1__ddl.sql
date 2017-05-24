93902adf5d519ced241051f75fdfe8d1b0fc2046
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-05-24 17:12:11 +0800

    delete flyway config

--- oauth2-server/src/main/resources/migration/V1_1__ddl.sql
+++ oauth2-server/src/main/resources/migration/V1_1__ddl.sql
@@ -21,9 +21,3 @@ CREATE TABLE `oauth_role_authority` (
   `authority` varchar(63) NOT NULL,
   KEY `index_role` (`role`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-INSERT INTO `oauth_client_details` VALUES ('hanyi', null, '$2a$10$Z9mdaAKWRRp4Bd9XjmbaLOLA/ugtmQBZ54J2UiIB.47.4wIjLn39m', 'read', 'client_credentials', null, 'ROLE_TRUSTED_CLIENT', '28800', null, null, null), ('OAuth2_root', ' ', '$2a$10$LsxuUXj95OybofiyUhmf7.uUm/3c8/t7TuzrRXQuB7GMoUGuF3oKS', 'read', 'client_credentials', ' ', ' ', '28800', null, null, null), ('test_client', '', '$2a$10$bYCZpDgAuvPnEUf5TUjv1O1mRBfwHvPe2V77MZvopJEuMSFlKJ3vy', 'read', 'client_credentials', '', 'ROLE_TRUSTED_CLIENT', '28800', null, '{}', '');
-
-INSERT INTO `oauth_role_authority` VALUES ('ROLE_TRUSTED_CLIENT', 'a'), ('ROLE_TRUSTED_CLIENT', 'c'), ('ROLE_TRUSTED_CLIENT', 'b'), ('role_b', 'a'), ('ROLE_TRUSTED_CLIENT', 'e');
-
