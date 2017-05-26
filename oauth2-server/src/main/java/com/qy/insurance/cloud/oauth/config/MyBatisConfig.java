d9f98784f14fdf90bdb31069883508f1e7ff1eab
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-05-26 09:36:40 +0800

    配置修改

--- oauth2-server/src/main/java/com/qy/insurance/cloud/oauth/config/MyBatisConfig.java
+++ oauth2-server/src/main/java/com/qy/insurance/cloud/oauth/config/MyBatisConfig.java
@@ -1,6 +1,7 @@
 package com.qy.insurance.cloud.oauth.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.flywaydb.core.Flyway;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
