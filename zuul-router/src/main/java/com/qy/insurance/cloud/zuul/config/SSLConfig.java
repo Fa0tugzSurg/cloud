6b85a2cc53e3b7de45279a95179204d88c56ea1b
Fa0tugzSurg <fanzhangjiaben@126.com>
2017-08-07 14:29:20 +0800

    test

--- zuul-router/src/main/java/com/qy/insurance/cloud/zuul/config/SSLConfig.java
+++ zuul-router/src/main/java/com/qy/insurance/cloud/zuul/config/SSLConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({com.qy.insurance.cloud.core.security.ssl.DefaultSslConfig.class
         , com.qy.insurance.cloud.core.security.ssl.EurekaSslConfig.class})
+
 public class SSLConfig {
 
 }
